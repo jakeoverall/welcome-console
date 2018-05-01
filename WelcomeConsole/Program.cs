d8bd19a76b914c558491ca0cf07bf13cb04f0c42
Jake Overall <joverall22@gmail.com>
2018-04-30 21:00:32 -0600

    welcome msg

--- WelcomeConsole/Program.cs
+++ WelcomeConsole/Program.cs
@@ -6,7 +6,7 @@ namespace WelcomeConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Visual Studio");
         }
     }
 }
